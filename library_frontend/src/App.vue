980c58019fc08ded7d0367f75f9ad57fa3c86a08
tandieuduc <ductandieu456@gmail.com>
2025-02-28 07:27:13 +0700

    Khoi tao du an

---
+++ library_frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+
+}
+</script>
+
+
+<template>
+  <h1>Hello, Welcome to the library Application!</h1>
+</template>
+
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
